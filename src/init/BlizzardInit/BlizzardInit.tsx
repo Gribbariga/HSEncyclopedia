2bdb95396bee6a909ec4908686be3ae4f208ab5f
Gribbariga <mormir09@gmail.com>
2022-12-21 11:38:33 +0300

    get token

--- src/init/BlizzardInit/BlizzardInit.tsx
+++ src/init/BlizzardInit/BlizzardInit.tsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-
 const BlizzardInit = () => {
   const fetchAccessToken = async () => {
-    console.log(process.env.BLIZZARD_CLIENT_ID);
-    console.log(process.env.BLIZZARD_CLIENT_SECRET);
+
     const response = await axios.post(
       "https://oauth.battle.net/token",
       new URLSearchParams({
@@ -13,11 +11,9 @@ const BlizzardInit = () => {
       {
         auth: {
           username:
-            process.env.BLIZZARD_CLIENT_ID ||
-            "0eec3527dfa1420993973710884a923e",
+          process.env.NEXT_PUBLIC_BLIZZARD_CLIENT_ID || "" ,
           password:
-            process.env.BLIZZARD_CLIENT_SECRET ||
-            "fFQpD4iO8NxSiKd4d0m8ZuyJdL3YhWGX",
+          process.env.NEXT_PUBLIC_BLIZZARD_CLIENT_SECRET || ""
         },
       },
     );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import type { AppProps } from "next/app";
+// import type { AppProps } from "next/app";
+import { store } from "@/store/index";
 import MainLayoyt from "@/layout/MainLayoyt";
 import { GlobalStyles } from "@/styles/GlobalStyle";
 import InitComponents from "init/InitComponents";
-import { store } from "@/store/index";
-import { Provider } from "react-redux/es/exports";
+
+
+import { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log(process.env);
   return (
     <>
       <GlobalStyles />
